2be2779836159591164217dce7caacf873621246
Bruno Sonnino <sonnino@revolution.com.br>
2016-11-27 10:35:35 -0200

    Add project data

--- ProcessJson/MainPage.xaml.cs
+++ ProcessJson/MainPage.xaml.cs
@@ -1,30 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ProcessJson
 {
+    public class JsonDecoded
+    {
+        public JsonDecoded()
+        {
+            Children = new List<JsonDecoded>();
+        }
+
+        public string Key { get; set; }
+        public List<JsonDecoded> Children { get; }
+    }
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void FormatJsonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(MiniBox.Text))
+                {
+                    FormBox.Text = JsonConvert.SerializeObject(
+                        JsonConvert.DeserializeObject(MiniBox.Text), Formatting.Indented);
+                }
+            }
+            catch (JsonReaderException ex)
+            {
+                await new MessageDialog($"Error parsing JSON: {ex.Message}").ShowAsync();
+            }
+        }
+
+        private async void MinifyJsonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(FormBox.Text))
+                {
+                    MiniBox.Text = JsonConvert.SerializeObject(
+                        JsonConvert.DeserializeObject(FormBox.Text), Formatting.None);
+                }
+            }
+            catch (JsonReaderException ex)
+            {
+                await new MessageDialog($"Error parsing JSON: {ex.Message}").ShowAsync();
+            }
         }
     }
 }
